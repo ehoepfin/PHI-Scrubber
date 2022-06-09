e88c564cb96bc091503c69e260f0af5c105246d7
Leighanna Pipatanangkura <leighanna.pipatanangkura@nvoq.com>
2022-06-08 18:01:17 -0600

    add NER recognition

--- scrubber.py
+++ scrubber.py
@@ -6,15 +6,30 @@ Created on Wed Jun  8 11:42:29 2022
 """
 import spacy
 
+# input and output files
+input_file = 'PHIScrubber.txt'
+output_file = "Scrubbed_" + input_file
+
 """ Load the Spacy model, use the testing corpus we created"""
 nlp = spacy.load("en_core_web_sm")
 
 """ opens the .txt file and saves it as a string"""
-with open('PHIScrubber.txt') as f:
-    contents = f.read()
+with open(input_file, 'r') as f:
+    text_to_scrub = f.read()
     
-doc = nlp(contents)
+doc = nlp(text_to_scrub)
 
-#Prints the POS tag in the terminal
-for token in doc:
-    print(token.text, token.tag_)
+# Performs NER
+for ent in doc.ents:
+  print(ent.text,ent.label_)
+
+# replace the NER tagged as 'PERSON'
+scrubbed_text = text_to_scrub
+for ent in reversed(doc.ents):
+    if ent.label_ == 'PERSON':
+        scrubbed_text = scrubbed_text[:ent.start_char] +ent.label_ + scrubbed_text[ent.end_char:]
+
+with open(output_file, 'w') as f:
+    f.write(scrubbed_text)
+    
+print(scrubbed_text)
